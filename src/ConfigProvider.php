463cab32f11cc4471eed8049b39755b5a4a9aeb8
李铭昕 <715557344@qq.com>
2021-07-18 13:23:29 +0800

    Split `service-governance` into `service-governance` and `service-governance-consul`.

---
+++ src/ConfigProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\ServiceGovernanceConsul;
+
+use Hyperf\ServiceGovernanceConsul\Listener\RegisterDriverListener;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                ConsulAgent::class => ConsulAgentFactory::class,
+            ],
+            'listeners' => [
+                RegisterDriverListener::class,
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => 'The config of service governance.',
+                    'source' => __DIR__ . '/../publish/services.php',
+                    'destination' => BASE_PATH . '/config/autoload/services.php',
+                ],
+            ],
+        ];
+    }
+}
